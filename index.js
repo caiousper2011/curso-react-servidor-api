69c7e640d2c82d977a7006e14c39b61221351428
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-01-13 12:45:27 -0300

    adding helmet to the project

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const PORT = 3000;
 const app = express();
 
-app.use(cors()).use(bodyParser.json());
+app.use(helmet()).use(cors()).use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`servidor online maluco, na porta: ${PORT}`);
